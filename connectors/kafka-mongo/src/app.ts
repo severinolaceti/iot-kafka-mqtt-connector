7c014112b75291024649e9057d3b2648416523da
Flaviano Dias <flavindias@msn.com>
2021-05-03 00:28:13 -0300

    ✨ Mongo Connector (kafka->mongo)

---
+++ connectors/kafka-mongo/src/app.ts
@@ -0,0 +1,59 @@
+import dotenv from 'dotenv';
+import { Kafka } from 'kafkajs';
+import {MongoClient} from 'mongodb';
+dotenv.config();
+
+const { KAFKA_BROKERS, KAFKA_TOPICS, MONGO_URL, DB_NAME } = process.env;
+const dbClient = new MongoClient(MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+dbClient.connect(async (err) => {
+	if (err) throw err;
+	console.log('MongoDB has been connected');
+});
+const kafka = new Kafka({
+	clientId: 'kafka-mongo-connector',
+	brokers: KAFKA_BROKERS.split(',')
+});
+
+const consumer = kafka.consumer({ groupId: 'kafka-mongo-connector' });
+
+const run = async () => {
+	try {
+		await consumer.connect();
+		KAFKA_TOPICS.split(',').map(async(topic) => {
+			await consumer.subscribe({ topic, fromBeginning: true });
+		});
+
+		await consumer.run({
+			eachMessage: async ({ topic, partition, message }) => {
+				const data = JSON.parse(message.value.toString()).Data;
+				const tzCloud = JSON.parse(message.value.toString()).TimestampCloud;
+				const tz = `${JSON.parse(message.value.toString()).Timestamp}`.length === 19 ? 
+					`${JSON.parse(message.value.toString()).Timestamp}.000Z` : 
+					JSON.parse(message.value.toString()).Timestamp;
+
+				const result = await dbClient
+					.db(DB_NAME)
+					.collection(topic)
+					.insertOne({
+						...data,
+						bess: message.key.toString(),
+						createdAt: tzCloud ? tzCloud : tz,
+						updatedAt: tzCloud ? tzCloud : tz
+					});
+				console.log(result.insertedCount);
+				if(result.insertedCount === 1){
+					console.log(new Date().toISOString(), partition, message.key.toString(), topic);
+				}
+			},
+		});
+
+	}
+	catch(error){
+		console.error(error);
+	}
+};
+run();
